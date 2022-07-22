f388bdcbd9480a44a9fc8c9100fbee5b2b208cd2
Mr-G-D <daredinesh00@gmail.com>
2022-07-22 07:21:04 +0530

    Rich text editor implemented

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./index.css";
+import { QuillEditor } from "@vueup/vue-quill";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(QuillEditor).mount("#app");
--- src/views/Discussion/index.vue
+++ src/views/Discussion/index.vue
@@ -1,15 +1,18 @@
 <template>
   <HeaderVue />
-  Discussion
+  <QuillEditor theme="snow" toolbar="full" />
 </template>
 
 <script>
 import HeaderVue from "../../Layouts/Header.vue";
+import { QuillEditor } from "@vueup/vue-quill";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
 
 export default {
   name: "DiscussionVue",
   components: {
     HeaderVue,
+    QuillEditor,
   },
 };
 </script>
